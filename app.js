8b59cc62fa656d08be5579e6782ee8890d7e52a6
DevvSakib <devvsakib@gmail.com>
2022-09-27 00:42:22 +0600

    Update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const unlockLock = document.querySelector('.lockUnlock'),
     pass = document.querySelector('#pass'),
     lockAgain = document.querySelector('.lock'),
+    downArrow = document.querySelector('.downArrow'),
     submit = document.querySelector('#submit')
 
 
@@ -17,8 +18,6 @@ submit.onclick = () => {
         unlockLock.classList.add('hide');
         unlockLock.style.opacity = "0";
         unlockLock.style.transition = "1200ms cubic-bezier(0.25, 0.46, 0.45, 0.94)";
-
-        
         document.body.style.overflow = "auto";
         pass.value = ""
     }
@@ -32,4 +31,18 @@ submit.onclick = () => {
 lockAgain.onclick = () => {
     unlockLock.classList.remove('hide');
     document.body.style.overflow = "hidden"
+    unlockLock.style.opacity = "1";
+
+}
+window.onscroll = () => {
+    console.log(document.body.scrollTop);
+    if (window.scrollY <= 20) {
+        downArrow.style.opacity = '1';
+        downArrow.style.transition = 'opacity 1000ms ease-in-out';
+
+    } else {
+        downArrow.style.opacity = '0';
+        downArrow.style.transition = '1000ms ease-in-out';
+
+    }
 }
