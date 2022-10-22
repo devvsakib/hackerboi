9ec1611eaef883f771bdb4102366e48f34d71f07
mridul-mb <mriravbagla@gmail.com>
2022-10-22 11:42:40 +0530

    contributors are visible on contributors.html page

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
-fetch("./contributors.json").
+
+
+let users = fetch("./contributors.json").
     then(response => response.json()).
-    then(data => console.log(data))
+    then(data => {
+        let users = data.profiles;
+        let element = users.map((user) => (
+            `<div class="contributor">
+                <div class="head-contributor">
+                    <img src=${user.avatarUrl} alt=${user.avatarUrl} width="250px">
+                </div>
+                <div class="body-contribute">
+                    <div class="name">${user.name}</div>
+                    <div class="socials">
+                        <a href='https://github.com/${user.github}'><i class="fa-brands fa-github github"></i></a>
+                        <a href='https://twitter.com/${user.twitter}'><i class="fa-brands fa-twitter twitter"></i></a>
+                    </div>
+                </div>
+            </div>`
+        ))
+
+        let child = document.createElement('div');
+        child.className = 'contributors'
+        child.innerHTML = element;
+
+        let contributors = document.querySelector('.mapping-contributors');
+        contributors.appendChild(child);
+        return data;
+
+    }
+    )
+
+
+
+
+
